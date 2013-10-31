3d887a59a9e99cad216bc25b58167c728266f2e4
elotmany <mohammed.elotmany@insbaixcamp.cat>
2013-10-31 19:50:40 +0100

    Creació calculadora simple

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -1,7 +1,7 @@
 
 public class Calculadora {
 	public static void main(String[] args) {
-		System.out.println(" Sóc una calculadora científica" );
+		System.out.println("Sóc una calculadora Simple" );
 	}
 
 }
